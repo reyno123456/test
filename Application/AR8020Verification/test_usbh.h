006b120116e93f8c1f34a06f76607980aadc39b0
jjxiong <jjxiong@embedded.(none)>
2016-09-05 15:20:07 +0800

    add usb host application code

---
+++ Application/AR8020Verification/test_usbh.h
@@ -0,0 +1,33 @@
+#ifndef __TEST__USBH__H
+#define __TEST__USBH__H
+
+#include <stdio.h>
+#include "usbh_def.h"
+
+typedef enum
+{
+    FILE_OPERATION_IDLE = 0,
+    FILE_OPERATION_START,
+    FILE_OPERATION_BUSY,
+    FILE_OPERATION_DISPLAY,
+}FileOperationState;
+
+
+typedef struct _FileOperationContext
+{
+    volatile FileOperationState state;
+    volatile uint16_t            operated;
+}FILE_OPERATION_CONTEXT;
+
+
+void test_OperateFile(void);
+void test_DisplayFile(char *path, uint8_t recuLevel);
+void test_ProcessOperation(void);
+void USBH_UserPorcess(USBH_HandleTypeDef *phost, uint8_t id);
+void test_usbh(void);
+
+
+
+
+#endif
+
