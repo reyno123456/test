c56b5f63a085c0c1fcfd0160cc75e3f33f7ecd04
Liu Jing <jing.liu@artosyn.cn>
2016-09-26 19:02:23 +0800

    Add "sky" and "ground" application scenario seperate build

---
+++ Application/AR8020Verification/cpu2/main_sky.c
@@ -0,0 +1,28 @@
+#include "debuglog.h"
+#include "serial.h"
+#include "adv_7611.h"
+#include "h264_encoder.h"
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+    int tmp;
+  
+    (*(volatile unsigned int *)0x40B0008C) = 0x00500000; //PATCH for FPGA version, PIN MUX for UART9
+   
+    serial_init(9, 115200);
+    dlog_info("main function start \n");
+    
+    command_init();
+
+    ADV_7611_Initial();
+    H264_Encoder_Init();
+
+    for( ;; );
+} 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
