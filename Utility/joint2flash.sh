106a2ac4c8916d10d9246f071ae0f194c85f1742
mzhao <min.zhao@artosyn.cn>
2016-09-27 02:09:58 +0800

    add the exe tool to transform the txt format file into binary format file

--- Utility/joint2flash.sh
+++ Utility/joint2flash.sh
@@ -3,6 +3,11 @@
 # Date: 2016-9-23
 # Verison: 0.1
 # This script is used to cat cpu* binary file into one flash image
+# Note: if you run this script and get the following error on linux: 
+#       /bin/bash^M bad interpreter:No such file or directory
+# you could do like this to convert the file to linux line-ending format:
+#       vi joint2flash.sh
+#       :set ff=unix and :wq 
 
 function helptext()
 {
@@ -40,22 +45,20 @@ do
 	esac
 done
 
+outputtxt=flash.txt
 output=$6
-#change the binary to flash txt
-#./bin2flash.exe  -f #cpu0 -o cpu0
-#./bin2flash.exe  -f $cpu1 -o cpu1
-#./bin2flash.exe  -f $cpu2 -o cpu2
+
 
 #delete the line address of flash txt
-sed -i 's/@.\{8\} \{3\}//g' $cpu0
-sed -i 's/@.\{8\} \{3\}//g' $cpu1
-sed -i 's/@.\{8\} \{4\}//g' $cpu2
+sed -i 's/@.\{9\} \{3\}//g' $cpu0
+sed -i 's/@.\{9\} \{3\}//g' $cpu1
+sed -i 's/@.\{9\} \{3\}//g' $cpu2
 
 #get the length of cpu1/cpu2.txt
 cpu1length=`cat $cpu1 | wc -l`
 cpu2length=`cat $cpu2 | wc -l`
 
-cat $cpu0 > $output
+cat $cpu0 > $outputtxt
 
 #add size of cpu1 to flash.image
 for i in {0..3}
@@ -64,10 +67,10 @@ do
 	tmp=`echo $cpu1length $shiftlen | awk '{print rshift($1,$2)}'`
 	tmp=`echo $tmp | awk '{print and($1,255)}'`
 	tmphex=$(dec2hex $tmp)
-	echo $tmphex >> $output
+	echo $tmphex >> $outputtxt
 done
 
-cat $cpu1 >> $output
+cat $cpu1 >> $outputtxt
 
 #add size of cpu2 to flash.image
 for i in {0..3}
@@ -76,6 +79,9 @@ do
 	tmp=`echo $cpu2length $shiftlen | awk '{print rshift($1,$2)}'`
 	tmp=`echo $tmp | awk '{print and($1,255)}'`
 	tmphex=$(dec2hex $tmp)
-	echo $tmphex >> $output
+	echo $tmphex >> $outputtxt
 done
-cat $cpu2 >> $output
+cat $cpu2 >> $outputtxt
+
+#transfer the ascii format to hexadecimal
+../../Utility/txt2bin.exe  -i $outputtxt -o $output
