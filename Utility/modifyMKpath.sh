0ef0e1b4a0aa546ef378df80f694886f20703822
mzhao <min.zhao@artosyn.cn>
2016-12-19 11:54:05 +0800

    modified the shell script to replace the path in joint2flash.sh when compile in sdk

--- Utility/modifyMKpath.sh
+++ Utility/modifyMKpath.sh
@@ -15,6 +15,9 @@ sed -i '/^TOP_DIR ?= /cTOP_DIR ?= ..'  ./Makefile
 sed -i '/^OUTPUT_DIR ?= /cOUTPUT_DIR ?= $(TOP_DIR)'  ./Makefile
 sed -i '/^OUTPUT_HEADER_STAGING_DIR ?= /cOUTPUT_HEADER_STAGING_DIR ?= $(OUTPUT_DIR)/Inc'  ./Makefile
 sed -i '/^OUTPUT_LIB_STAGING_DIR ?= /cOUTPUT_LIB_STAGING_DIR ?= $(OUTPUT_DIR)/Lib'  ./Makefile
+sed -i '/cp \*\.bin/d' ./Makefile
+
+
 
 #modify the cpu0 sample top makefile
 sed -i '/^TOP_DIR ?= /cTOP_DIR ?= ../..'  ./cpu0/Makefile
@@ -28,11 +31,19 @@ sed -i '/^OUTPUT_DIR ?= /cOUTPUT_DIR ?= $(TOP_DIR)'  ./cpu1/Makefile
 sed -i '/^OUTPUT_HEADER_STAGING_DIR ?= /cOUTPUT_HEADER_STAGING_DIR ?= $(OUTPUT_DIR)/Inc'  ./cpu1/Makefile
 sed -i '/^OUTPUT_LIB_STAGING_DIR ?= /cOUTPUT_LIB_STAGING_DIR ?= $(OUTPUT_DIR)/Lib'  ./cpu1/Makefile
 
-#modify the cpu0 sample top makefile
+#modify the cpu2 sample top makefile
 sed -i '/^TOP_DIR ?= /cTOP_DIR ?= ../..'  ./cpu2/Makefile
 sed -i '/^OUTPUT_DIR ?= /cOUTPUT_DIR ?= $(TOP_DIR)'  ./cpu2/Makefile
 sed -i '/^OUTPUT_HEADER_STAGING_DIR ?= /cOUTPUT_HEADER_STAGING_DIR ?= $(OUTPUT_DIR)/Inc'  ./cpu2/Makefile
 sed -i '/^OUTPUT_LIB_STAGING_DIR ?= /cOUTPUT_LIB_STAGING_DIR ?= $(OUTPUT_DIR)/Lib'  ./cpu2/Makefile
 
-
+#modify the joint2flash.sh
+cd $SDK_DIR/Utility
+echo `pwd`
+sed -i '/^bootload=/cbootload=../Lib/ar8020_boot.bin'  ./joint2flash.sh
+sed -i '/^upgrade=/cupgrade=../Lib/ar8020_upgrade.bin'  ./joint2flash.sh
+sed -i '/^cpu0=/ccpu0=../Lib/ar8020_cpu0.bin'  ./joint2flash.sh
+sed -i '/^cpu1=/ccpu1=../Lib/ar8020_cpu1.bin'  ./joint2flash.sh
+sed -i '/^cpu2=/ccpu2=../Lib/ar8020_cpu2.bin'  ./joint2flash.sh
+sed -i '/^ve=/cve=../Utility/imageinfo'  ./joint2flash.sh
 
