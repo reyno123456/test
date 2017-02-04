38e56c79041d7b517a7a2efb5613f544aed1237d
Xiong Jiangjiang <jjxiong@artosyn.com>
2017-02-04 10:35:52 +0800

    fix the compile problem

--- Application/Apollo1/cpu1/main.c
+++ Application/Apollo1/cpu1/main.c
@@ -5,6 +5,7 @@
 #include "stm32f746xx.h"
 #include "test_usbh.h"
 #include "hal_usb_host.h"
+#include "hal_usb_otg.h"
 #include "hal_sys_ctl.h"
 
 /**
@@ -43,7 +44,7 @@ int main(void)
 
     CPU_CACHE_Enable();
 
-    HAL_USB_InitHost(HAL_USB_HOST_PORT_1, HAL_USB_HOST_CLASS_UVC);
+    HAL_USB_InitHost(HAL_USB_PORT_1, HAL_USB_HOST_CLASS_UVC);
 
     /* We should never get here as control is now taken by the scheduler */
     for( ;; )
