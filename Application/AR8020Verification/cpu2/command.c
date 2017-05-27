043210a36e9c9fc19cf6ddc1df522f16c3082224
Wu Min <min.wu@artosyn.com>
2017-05-27 18:31:30 +0800

    zhongyu ma update

--- Application/AR8020Verification/cpu2/command.c
+++ Application/AR8020Verification/cpu2/command.c
@@ -21,21 +21,9 @@ void command_run(char *cmdArray[], uint32_t cmdNum)
 {
     extern int BB_add_cmds(uint8_t type, uint32_t param0, uint32_t param1, uint32_t param2);
     
-    if (memcmp(cmdArray[0], "BB_data_buf_full", strlen("BB_data_buf_full")) == 0)
-    {
-        command_TestSysEventInterCore(cmdArray[1]);
-    }
-    else if (memcmp(cmdArray[0], "BB_uart10_spi_sel", strlen("BB_uart10_spi_sel")) == 0)
+    if (memcmp(cmdArray[0], "BB_uart10_spi_sel", strlen("BB_uart10_spi_sel")) == 0)
     {
         BB_uart10_spi_sel( strtoul(cmdArray[1], NULL, 0) );
-    }
-    else if (memcmp(cmdArray[0], "test_SysEventIdle", strlen("test_SysEventIdle")) == 0)
-    {
-        command_TestSysEventIdle();
-    }
-    else if (memcmp(cmdArray[0], "test_float_calculate_pi", strlen("test_float_calculate_pi")) == 0)
-    {
-        test_float_calculate_pi();
     }
     else if(memcmp(cmdArray[0], "BB_add_cmds", strlen("BB_add_cmds")) == 0)
     {
@@ -48,15 +36,7 @@ void command_run(char *cmdArray[], uint32_t cmdNum)
     else if (memcmp(cmdArray[0], "help", strlen("help")) == 0) 
     {
         dlog_error("Please use commands like:");
-        dlog_error("BB_data_buf_full <R>");
-        dlog_error("BB_debug_print_init_sky");
-        dlog_error("BB_debug_print_init_grd");
-        dlog_error("BB_SPI_ReadByte <page> <addr>");
-        dlog_error("BB_SPI_WriteByte <page> <addr> <value>");
         dlog_error("BB_uart10_spi_sel <value>");
-        dlog_error("test_nor_flash_all <flash start address> <size> <value>");
-        dlog_error("test_SysEventIdle");
-        dlog_error("test_float_calculate_pi");
         dlog_error("BB_add_cmds <type> <param0> <param1> <param2>");		
         dlog_output(1000);	
     }
